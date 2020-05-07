2c226f67c5ade5ef526b7a013dc1fbe67da1b7e3
Alex <alexdeassis7@gmail.com>
2020-05-06 22:10:12 -0300

    Clase 1 Suma de dos numeros

---
+++ Clase1HolaMundo/src/clase1holamundo/Clase1HolaMundo.java
@@ -0,0 +1,23 @@
+package clase1holamundo;
+//Soy un comentario de linea 
+
+import java.util.Scanner;
+
+public class Clase1HolaMundo {
+
+    public static void main(String[] args) {
+        //definimos e inicializamos     
+        int num1 = 0, num2 = 0, resultado = 0;
+        Scanner teclado = new Scanner(System.in);
+        //solicitamos datos al usuario
+        System.out.println("Ingrese el numero 1");
+        num1 = teclado.nextInt();
+        
+        System.out.println("Ingrese el numero 2");
+        num2 = teclado.nextInt();
+        //procesamiento de la info 
+         resultado = num1 + num2 ;
+       //mostramos la info 
+        System.out.println("El resultado de la suma es " + resultado);    
+   }
+}
